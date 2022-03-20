481eb0ee9c2b0b09640e8ad4a63d3d70e641165b
Josué Chaves Araya <d4n11083@estudiantec.cr>
2022-03-20 17:03:11 -0600

    feature: Esqueleto de A*
    "Machote" para el algoritmo de A*, Faltan hacer las funciones auxiliares, que están listadas antes del codigo del algortimo

--- js/Juego.js
+++ js/Juego.js
@@ -38,6 +38,7 @@ class Juego {
         return false;
     }
 
+
     getPosPiezaBlanca() {
         for (let i = 0; i < 3; i++) {
             for (let j = 0; j < 3; j++) {
@@ -46,6 +47,48 @@ class Juego {
                 }
             }
         }
+    }
+
+    //Todo: 
+    //Definir que va a ser el objeto estado
+    //  Puede ser otra clase con las funciones de g y h
+    //Función para calcular los hijos de un estado 
+    //Función para calcular cuando ya se llegó al estado objetivo
+    
+
+
+    algoritmoAEstrella() {
+
+        var listaEstados = 0                           //Acá se inicializa la cola de prioridad 
+        var camino = []                                //Camino que se debe recorrer para llegar a la solucion
+
+        // estados.push( { puzzle: estado, distancia:0 } ) //Acá se ingresa el primer estado, o puzzle 
+                                                       // que es el la matriz que hay que resolver 
+                                                       // Notese que la distancia es 0, ya que aún no 
+                                                       // se ha movido la pieza vacia. 
+
+
+        while( estados.size() > 0){                              //Acá empieza el algoritmo, 
+            // var estadoActual = listaEstados.pop().puzzle      //Acá devuelve el estado con menor f de la lista de estados 
+
+            if( estadoActual = matrizObjetivo ){ //Reviza si ya llegó a la matriz objetivo 
+                return camino
+            }
+
+            var hijos = []; //Acá hay que calcular los estados hijos 
+                           // osea todos los movimientos posibles del estadoActual
+            
+            hijos.forEach(hijo => {                                //"Por cada hijo de la lista hijos"
+                // var f = calcularG(hijo) + calcularH(hijo)       // Se calcula g y h, acá H puede ser manhattan 
+                // listaEstados.push({puzzle: hijo, distancia: f}) //Se ingresan en la lista de estados 
+                console.log("hijo");
+            })
+            
+
+        }
+
+
+
     }
 
 
