fd171c5cfd7842d55d4b6d751a622e9eb7baa551
Hansol A. Rostrán <89949216+hros19@users.noreply.github.com>
2022-03-31 18:46:29 -0600

    Fixed Double-Click bug

--- js/index.js
+++ js/index.js
@@ -1,19 +1,16 @@
 
 //Matrices de prueba
-
 matrizObjetivo = [
   [1, 2, 3],
   [4, 5, 6],
   [7, 8, 0]
 ]
 
-
 matrizEjercicio1 = [
   [2, 1, 3],
   [4, 5, 6],
   [8, 7, 0]
 ]
-
 
 let juegoMain = new Juego(matrizObjetivo)
 var selectedAlgorithm = 'A*'   //'A*' ejecuta AEstrella  'Back' ejecuta Backtracking
@@ -29,19 +26,18 @@ getRowColumn = (matrix, search) =>{
   return [row,col]
 }
 
-
 //Intercambia el número en la celda, con la posicion del original en la matriz 
 swapCellInfo = ( originalNumber, currentNumber, originalMatrix ) => {
-
   originalPos = getRowColumn(originalMatrix, originalNumber)  
   currentPos = getRowColumn(originalMatrix, currentNumber)
-
   var newMatrix = originalMatrix.map( arr => arr.slice() )                  
-
   newMatrix[originalPos[0]][originalPos[1]] = currentNumber
   newMatrix[currentPos[0]][currentPos[1]] = originalNumber
-
   setDocumentTable(newMatrix, 'table_Puzzle')
+}
+
+getCoordinatesOfPiece = (matrix, piece) => {
+    return matrix[row][column];
 }
 
 //Se encarga de cambiar el valor de la celda 
@@ -52,31 +48,35 @@ setCellInfo = (input, originalMatrix) => {
 
   td.removeAttribute('data-clicked')
   td.removeAttribute('data-text')
-  if( isNaN(currentText) ){
+  //console.log("O: ", originalText)
+  //console.log("C: ", currentText)
+  if ( isNaN(currentText) ) {
     alert('No puede Ingresar Texto')
-
-  }else if( parseInt(currentText) > 8 || parseInt(currentText) < 0 ){
+  } 
+  else if( parseInt(currentText) > 8 || parseInt(currentText) < 0 ) {
     alert('Debe ingresar un número entre 0-8')
-
-  }else if( originalText != currentText ){
-
+  } 
+  //else if (!currentText in ["1","2","3","4","5","6","7","8","0"]) {
+      //alert('No puede ingresar un número fuera de la matriz')
+      //return
+  //}
+  else if( originalText != currentText ) {
     swapCellInfo(parseInt(originalText), parseInt(currentText), originalMatrix)
     return
   }
-
   td.innerHTML = originalText
   console.log('No se realizaron cambios');
-
 }
 
 //Función que agrega un input a la celda de la tabla, para editar su contenido 
 setCellEditable = (htmlElement) => {
-
   if (!isStepByStep) {
-    if( htmlElement.hasAttribute('data-clicked') ){
+    if ( htmlElement.hasAttribute('data-clicked') ) {
       return
     }
-    
+    if (htmlElement.innerHTML in ["0","1","2","3","4","5","6","7","8"] == false) {
+        return;
+    }
     htmlElement.setAttribute('data-cliked','yesy')
     htmlElement.setAttribute('data-text', htmlElement.innerHTML)
     
@@ -85,42 +85,38 @@ setCellEditable = (htmlElement) => {
     var input = document.createElement('input')
     input.setAttribute('type','text')
     input.value = htmlElement.innerHTML
+    console.log(input, input.value)
     input.onblur = function(){setCellInfo(input, originalMatrix)}  //Llama la función de cambiar el contenido cuando se da click en otro lado 
   
     htmlElement.innerHTML = ''
     htmlElement.append(input)
     htmlElement.firstElementChild.select()
-  }else{
+  }
+  else {
     console.log("No se puede editar en paso a paso");
   }
-
 }
 
 //Muestra la matriz en pantalla, utilizando la tabla en el html 
 //matrix: matriz a "imprimir"
 //documentTable: id de la tabla en el HTML
 setDocumentTable = (matrix, documentTable) => {
-
   var documentTable = document.getElementById(documentTable) //Busca el elemento tabla en el HTML
-
-  
   //Verifica si table tiene elementos, si los tiene los elimina. 
   while (documentTable.hasChildNodes()) {
     documentTable.removeChild(documentTable.firstChild);
   }
-  
   //Agrega los elementos de la matriz 
   matrix.forEach(row => { //Por cada fila en la matriz,
       var rowTable = document.createElement("tr") //Crea un elemento tr
 
       row.forEach(column => { //Por cada columna en la fila 
           var columnTable = document.createElement("td") //crea un elemento td
-          var columnText = document.createTextNode(column)
-
+          var columnText = document.createTextNode(column) //Crea un nodo de texto con el valor de la columna
           columnTable.appendChild(columnText) //Append del texto al td 
           columnTable.onclick = function(){ setCellEditable(this)} //EN FASE DE PRUEBA, acá agrega la función a cada celda
 
-          if(column == 0){
+          if (column == 0) {
             columnTable.setAttribute('class', 'blank_piece')
           }
           rowTable.appendChild(columnTable) //Apend del td al tr 
@@ -137,12 +133,10 @@ getMatrizDocument = (nombreTablaHTML) => {
   var tableObj = document.getElementById( nombreTablaHTML );
   var arr = [];
   var allTRs = tableObj.getElementsByTagName( "tr" );
-  for ( var trCounter = 0; trCounter < allTRs.length; trCounter++ )
-  {
+  for ( var trCounter = 0; trCounter < allTRs.length; trCounter++ ) {
     var tmpArr = [];
     var allTDsInTR = allTRs[ trCounter ].getElementsByTagName( "td" );
-    for ( var tdCounter = 0; tdCounter < allTDsInTR.length; tdCounter++ )
-    {
+    for ( var tdCounter = 0; tdCounter < allTDsInTR.length; tdCounter++ ) {
         var digit = parseInt( allTDsInTR[ tdCounter ].innerHTML )
         tmpArr.push( digit );
     }
@@ -154,19 +148,18 @@ getMatrizDocument = (nombreTablaHTML) => {
 
 //Retorna la Solucion dependiendo del algoritmo seleccionado 
 const getSolution = () => {
-
-
   if (selectedAlgorithm == "A*") {
     console.log("Resolviendo con A*")  //Solo para probar que el juego funcione
-    return  juegoMain.algoritmoAEstrella()
-    
-  }else{
+    return  juegoMain.algoritmoAEstrella() 
+  }
+  else {
     console.log("Resolviendo con Bactracking")  //Solo para probar que el juego funcione
     solucionBack = juegoMain.algoritmoBacktracking()
     if (solucionBack != []) {
       alert("Es posible que el algoritmo no pueda resolver esta matriz")
       return false
-    }else{
+    }
+    else {
       return solucionBack
     }
   }    
@@ -175,32 +168,25 @@ const getSolution = () => {
 
 //
 const nextStep = () => {
-
-  
   console.log("Siguiente paso");
   isStepByStep = true
-  //Bloquear la Matriz 
-  // document.getElementById("table_Puzzle").classList.remove("enableTable") //
-  // document.getElementById("table_Puzzle").classList.add("disableTable") //
-  //alert("No puede editar la tabla mientras ejecuta el juego paso a paso ")
-  
+
   if (!Array.isArray(solucionPasoAPaso)) {
     juegoMain.tablero = getMatrizDocument('table_Puzzle')
-
     if (juegoMain.esTableroValido()) {
       solucionPasoAPaso = getSolution()
       setDocumentTable(solucionPasoAPaso.shift().estado,'table_Puzzle' )  //Muestra el Siguiente paso en la interfaz
       //Bloquear los botones de solución
-    }else{
+    }
+    else {
       alert("El tablero no tiene solución")
     } 
   }
   if (solucionPasoAPaso.length > 0) {
     setDocumentTable(solucionPasoAPaso.shift().estado,'table_Puzzle' )  //Muestra el Siguiente paso en la interfaz
-  }else{
+  } else {
     alert('Ya llegó a la solución')
   }
-
 }
 
 //Detiene la ejecución una cantidad de milisegundos 
@@ -228,7 +214,6 @@ const showSolution = async (camino) => {
     await sleep(100);
   }
   isStepByStep = false
-  
   runAgainButton.removeAttribute('disabled');
   changeAlgorithmBtn.removeAttribute('disabled');
   nextStepBtn.removeAttribute('disabled');
@@ -242,46 +227,40 @@ const runSolution = () => {
 
   if (juegoMain.esTableroValido()) {
     solucionPasoAPaso = 0 //Si ejecuta el paso a paso y se cansa y le da ejecutar, tonses resetea solucionPAP para que no vuelva de donde lo dejó 
-    if(!getSolution()){
+    if (!getSolution()) {
       console.log("No se pudo dar solución con backtracking");
-    }else{
-      showSolution(getSolution()) //Muestra la solucion paso a paso 
     }
-    
-  } else {
+    else {
+      showSolution(getSolution()) //Muestra la solucion paso a paso 
+    } 
+  }
+  else {
     alert("El tablero no tiene solución")
   }
- 
-
 }
 
 
 const changeAlgorithm = () => {
-  //solucionPasoAPaso = 0 //Si cambia el algoritmo, resetea el paso a pas//o No funcionó 
   var tableroHTML = document.getElementById( 'tablero' )
   var algNameHTML = document.getElementById('alg_Name')
 
-  if(selectedAlgorithm == 'A*'){
+  if (selectedAlgorithm == 'A*') {
     selectedAlgorithm = 'Back'
     console.log("Backtracking Seleccionado");  
-    
     tableroHTML.setAttribute('class', 'box_tablero bodyB')
     algNameHTML.innerHTML = 'Backtracking'
-    
-  }else{
+  }
+  else {
     selectedAlgorithm = "A*"
     console.log("A* Seleccionado");  
     tableroHTML.setAttribute('class', 'box_tablero bodyA')
     algNameHTML.innerHTML = 'A Estrella'
   }
-
-
 }
 
 const resetAll = () => {
   console.log("Reiniciando Juego");
   isStepByStep = false
-
   juegoMain = new Juego(matrizObjetivo)
   selectedAlgorithm = 'A*'   //'A*' ejecuta AEstrella  'Back' ejecuta Backtracking
   solucionPasoAPaso = 0
@@ -340,11 +319,9 @@ const obtenerPiezasMovibles = ( lista ) => {
 
 const checkMov = (lista, movimiento) => {
   var posiblesMovimientos = obtenerPiezasMovibles(lista)
-  for(let i = 0; i <posiblesMovimientos.length; i++){
-    
+  for(let i = 0; i <posiblesMovimientos.length; i++) {
     console.log(posiblesMovimientos[i]);
-    
-    if( posiblesMovimientos[i] == movimiento ){
+    if( posiblesMovimientos[i] == movimiento ) {
       return true
     }
   }
@@ -353,24 +330,19 @@ const checkMov = (lista, movimiento) => {
 
 
 const changeWBlanck = () => {
-  
   const number = document.getElementById("changeWBlank_Input")
-
-  if( number.value != '' && parseInt(number.value) > 0 && parseInt(number.value) <= 8 ){
-
-    if(checkMov(getMatrizDocument('table_Puzzle'), number.value)){
-
+  if ( number.value != '' && parseInt(number.value) > 0 && parseInt(number.value) <= 8 ) {
+    if (checkMov(getMatrizDocument('table_Puzzle'), number.value)) {
       swapCellInfo( 0, parseInt(number.value), getMatrizDocument('table_Puzzle') )
-      //juegoMain.tablero = getMatrizDocument('table_Puzzle')
       solucionPasoAPaso = 0 //Carga la nueva solución en la lista 
-    }else{
+    }
+    else {
       alert("Solo puede mover las piezas cercanas al 0 ")
     }
-
-  }else{
+  }
+  else {
     alert("Debe ingresar un número entre 1-8 en el input, para intercambiar con la pieza vacía")
   }
-
 }
 
 setDocumentTable(matrizEjercicio1, 'table_Puzzle');
